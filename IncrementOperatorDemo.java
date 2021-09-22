060d4dda05b3e9ce3923a0124ee688b0140430ef
Your Name <vishalvernekar1020@gmail.com>
2021-09-22 22:15:46 +0530

    writes increment operator demo program

---
+++ IncrementOperatorDemo.java
@@ -0,0 +1,14 @@
+class IncrementOperatorDemo {
+public static void main(String[] args){
+System.out.println("Preincrement operation........");
+int a = 1;
+int b = ++a;
+System.out.println("a :" +a);
+System.out.println("b :" +b);
+System.out.println("Postincrement operation........");
+int i = 10;
+int j = i++;
+System.out.println("i :" +i);
+System.out.println("j :" +j);
+}
+}
