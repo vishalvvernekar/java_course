4ebe9b0cd9b6fd64999d8370c19781a4b3d47d48
Your Name <vishalvernekar1020@gmail.com>
2021-09-22 21:26:59 +0530

    writes airthmatic operator demo program

---
+++ AirthmaticOperatorDemo.java
@@ -0,0 +1,24 @@
+class AirthmaticOperatorDemo {
+public static void main(String[] args){
+int a = 10;
+int b = 20;
+int addResult= a+b;
+System.out.println("addResult = :" +addResult);
+int p = 100;
+int q = 20;
+int subResult = p-q;
+System.out.println("subResult = :" +subResult);
+int x = 50;
+int y = 20;
+int mulResult = x*y;
+System.out.println("mulResult = :" +mulResult);
+int i = 5;
+int j = 10;
+int divResult = j/i;
+System.out.println("divResult = :" +divResult);
+int m = 9;
+int n = 2;
+int modResult = m/n;
+System.out.println("modResult = :" +modResult);
+}
+}
