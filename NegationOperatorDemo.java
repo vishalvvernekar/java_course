d84167fd328b15d9c1047cd297c804da425cddf1
Your Name <vishalvernekar1020@gmail.com>
2021-09-22 22:19:58 +0530

    writes negation operator demo program

---
+++ NegationOperatorDemo.java
@@ -0,0 +1,9 @@
+class NegationOperatorDemo {
+public static void main(String[] args){
+System.out.println("Preincrement operation........");
+boolean b = true;
+boolean result = !b;
+System.out.println("b :" +b);
+System.out.println("result :" +result);
+}
+}
